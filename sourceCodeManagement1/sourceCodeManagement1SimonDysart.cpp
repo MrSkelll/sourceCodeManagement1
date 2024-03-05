142ce4137a6b149b94291dd82fec59bc9014d382
dysart67166 <dysart67166@B124-27>
2024-03-05 09:08:45 -0500

    Add project files.

---
+++ sourceCodeManagement1/sourceCodeManagement1SimonDysart.cpp
@@ -0,0 +1,18 @@
+/**********************************************************************
+Simon Dysart
+03/05/2024
+V: 1.0
+Sinclair CIS 1202 101
+Purpose: Literally just hello world. (A blast to the past if you will!)
+**********************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World" << endl;
+
+	cin.get();
+	return 0;
+}
