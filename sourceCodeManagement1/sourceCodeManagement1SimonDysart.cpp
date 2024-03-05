e7a67481a8c29ba6776f2f395e4236bd6fab26f3
MrSkelll <129882700+MrSkelll@users.noreply.github.com>
2024-03-05 09:12:55 -0500

    sourceCodeManagement1SimonDysart.cpp
    I have changed "Hello, World" on line 14 to "Hello, Universe"

--- sourceCodeManagement1/sourceCodeManagement1SimonDysart.cpp
+++ sourceCodeManagement1/sourceCodeManagement1SimonDysart.cpp
@@ -1,7 +1,7 @@
 /**********************************************************************
 Simon Dysart
 03/05/2024
-V: 1.0
+V: 2.0
 Sinclair CIS 1202 101
 Purpose: Literally just hello world. (A blast to the past if you will!)
 **********************************************************************/
@@ -11,7 +11,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello, World" << endl;
+	cout << "Hello, Universe" << endl;
 
 	cin.get();
 	return 0;
